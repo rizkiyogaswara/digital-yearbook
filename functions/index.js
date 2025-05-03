29fb129f3f61182e27c24a145ad14b92b79b7ea8
rizki.yogaswara <rizki.yogaswara@gmail.com>
2025-05-03 14:38:47 +0700

    chore: register scheduled rotateFeaturedPhoto function in index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,13 @@
 const functions = require('firebase-functions');
 const app = require('./backend/server');
 const { incrementPhotoCountOnCreate, decrementPhotoCountOnDelete } = require('./triggers/photoTriggers');
+const { rotateFeaturedPhoto } = require('./backend/controllers/featuredController');
+
 
 exports.app = functions.https.onRequest(app);
 exports.incrementPhotoCountOnCreate = incrementPhotoCountOnCreate;
 exports.decrementPhotoCountOnDelete = decrementPhotoCountOnDelete;
+exports.rotateFeaturedPhoto = functions.pubsub
+  .schedule('1 17 * * 5') // Every Saturday at 00:01 AM Jakarta time (UTC+7)
+  .timeZone('Asia/Jakarta')
+  .onRun(rotateFeaturedPhoto);
